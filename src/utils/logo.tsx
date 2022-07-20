8dff636fbaa6af1f16952850d9843cab79abc2c2
Jarda Fišer <fiser@cngroup.dk>
2022-07-20 11:21:04 +0200

    change import of images

--- src/utils/logo.tsx
+++ src/utils/logo.tsx
@@ -1,25 +1,24 @@
-import { ReactComponent as CNGroup } from "./../assets/cngrouplogo.jpg";
-import { ReactComponent as Angular } from "./../assets/technologyIcons/angular-logo.png";
-import { ReactComponent as AWS } from "./../assets/technologyIcons/aws-logo.png";
-import { ReactComponent as Azure } from "./../assets/technologyIcons/azure-logo.png";
-import { ReactComponent as CSharp } from "./../assets/technologyIcons/c-sharp-logo.png";
-import { ReactComponent as Clojure } from "./../assets/technologyIcons/clojure-logo.png";
-import { ReactComponent as Drupal8 } from "./../assets/technologyIcons/drupal-8-logo.png";
-import { ReactComponent as Elm } from "./../assets/technologyIcons/elm-logo.png";
-import { ReactComponent as FSharp } from "./../assets/technologyIcons/f-sharp-logo.png";
-import { ReactComponent as Groovy } from "./../assets/technologyIcons/groovy-logo.png";
-import { ReactComponent as Java } from "./../assets/technologyIcons/java-logo.png";
-import { ReactComponent as Jenkins } from "./../assets/technologyIcons/jenkins-logo.png";
-import { ReactComponent as Kotlin } from "./../assets/technologyIcons/kotlin-logo.png";
-import { ReactComponent as Kubernetes } from "./../assets/technologyIcons/kubernetes-logo.png";
-import { ReactComponent as Moby } from "./../assets/technologyIcons/moby-logo.png";
-import { ReactComponent as NodeJS } from "./../assets/technologyIcons/nodejs-logo.png";
-import { ReactComponent as PHP } from "./../assets/technologyIcons/php-logo.png";
-import { ReactComponent as ReactLogo } from "./../assets/technologyIcons/react-logo.png";
-import { ReactComponent as Scala } from "./../assets/technologyIcons/scala-logo.png";
-import { ReactComponent as Spring } from "./../assets/technologyIcons/spring-logo.png";
-import { ReactComponent as Vue } from "./../assets/technologyIcons/vue-logo.png";
-
+import * as CNGroup from "../assets/cngrouplogo.jpg";
+import * as Angular from "./../assets/technologyIcons/angular-logo.png";
+import * as AWS from "./../assets/technologyIcons/aws-logo.png";
+import * as Azure from "./../assets/technologyIcons/azure-logo.png";
+import * as CSharp from "./../assets/technologyIcons/c-sharp-logo.png";
+import * as Clojure from "./../assets/technologyIcons/clojure-logo.png";
+import * as Drupal8 from "./../assets/technologyIcons/drupal-8-logo.png";
+import * as Elm from "./../assets/technologyIcons/elm-logo.png";
+import * as FSharp from "./../assets/technologyIcons/f-sharp-logo.png";
+import * as Groovy from "./../assets/technologyIcons/groovy-logo.png";
+import * as Java from "./../assets/technologyIcons/java-logo.png";
+import * as Jenkins from "./../assets/technologyIcons/jenkins-logo.png";
+import * as Kotlin from "./../assets/technologyIcons/kotlin-logo.png";
+import * as Kubernetes from "./../assets/technologyIcons/kubernetes-logo.png";
+import * as Moby from "./../assets/technologyIcons/moby-logo.png";
+import * as NodeJS from "./../assets/technologyIcons/nodejs-logo.png";
+import * as PHP from "./../assets/technologyIcons/php-logo.png";
+import * as ReactLogo from "./../assets/technologyIcons/react-logo.png";
+import * as Scala from "./../assets/technologyIcons/scala-logo.png";
+import * as Spring from "./../assets/technologyIcons/spring-logo.png";
+import * as Vue from "./../assets/technologyIcons/vue-logo.png";
 export {
   CNGroup,
   Angular,
